e456b6600f31acdb3fde0b5f59879f5be4fff25c
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-18 18:40:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,18 +3,15 @@ $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  phrase
 end
 
 def toadstool
   puts status
-  status
 end
 
 def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
-  phrase
 end
 
 def all_phrase
